1d6523bb67e03ab291eef0d33da9c2e58580d150
Othman Shareef <othmanosx@gmail.com>
2022-09-07 23:06:47 +0300

    Refactor: reset mock data

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -4,104 +4,71 @@ const data: NFTItem[] = [
   {
     id: 1,
     item: "Cool Cats #001",
-    properties: [{ id: 1, name: "Eyes", value: "Black" }],
-    status: "Complete",
+    properties: [],
+    status: "Incomplete",
     image: "/src/assets/cat-1.png",
   },
   {
     id: 2,
     item: "Cool Cats #002",
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
-    status: "Complete",
+    properties: [],
+    status: "Incomplete",
     image: "/src/assets/cat-2.png",
   },
   {
     id: 3,
     item: "Cool Cats #003",
     properties: [],
-    status: "Complete",
+    status: "Incomplete",
     image: "/src/assets/cat-3.png",
   },
   {
     id: 4,
     item: "Cool Cats #004",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
+    properties: [],
     status: "Incomplete",
     image: "/src/assets/cat-4.png",
   },
   {
     id: 5,
     item: "Cool Cats #005",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
-    status: "Complete",
+    properties: [],
+    status: "Incomplete",
     image: "/src/assets/cat-5.png",
   },
   {
     id: 6,
     item: "Cool Cats #006",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
+    properties: [],
     status: "Incomplete",
     image: "/src/assets/cat-6.png",
   },
   {
     id: 7,
     item: "Cool Cats #007",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
+    properties: [],
     status: "Incomplete",
     image: "/src/assets/cat-7.png",
   },
   {
     id: 8,
     item: "Cool Cats #008",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
-    status: "Complete",
+    properties: [],
+    status: "Incomplete",
     image: "/src/assets/cat-8.png",
   },
   {
     id: 9,
     item: "Cool Cats #009",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
-
+    properties: [],
     status: "Incomplete",
     image: "/src/assets/cat-9.png",
   },
   {
     id: 10,
     item: "Cool Cats #010",
-
-    properties: [
-      { id: 1, name: "Rarity", value: "Common" },
-      { id: 2, name: "Hair", value: "Black" },
-    ],
-
-    status: "Complete",
+    properties: [],
+    status: "Incomplete",
     image: "/src/assets/cat-10.png",
   },
 ]
